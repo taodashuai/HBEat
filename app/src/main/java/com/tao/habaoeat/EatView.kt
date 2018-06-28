8e918679491d6b8409e1aecca8ded0d5390bf3d3
Cris <wt@szcysoft.com>
2018-06-28 17:50:15 +0800

    /

--- app/src/main/java/com/tao/habaoeat/EatView.kt
+++ app/src/main/java/com/tao/habaoeat/EatView.kt
@@ -1,24 +1,37 @@
 package com.tao.habaoeat
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Point
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.DisplayMetrics
 import android.view.MotionEvent
 import android.view.View
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 /**
  * Created by wt on 2018/6/21.
  */
 class EatView : View {
 
-    /**
-     * 当前头坐标
-     */
+
+    val mRandom = Random()
+
+    lateinit var mFoodPosition: Point//食物的位置
+    var eatFood = true //食物是否已经被吃掉
+
+    lateinit var mPointList: ArrayList<Point>//主体
+
+    //当前方向 0，1，2，3 上，下，左，右
+    val UP = 1
+    val DOWN = 2
+    val LEFT = 3
+    val RIGHT = 4
+
+
     lateinit var position: Point
     lateinit var paint: Paint
     var windowX = 0f
@@ -26,14 +39,7 @@ class EatView : View {
     //宽度
     val eatWid = 20
 
-    //当前方向 0，1，2，3 上，下，左，右
     var nowDir = 0
-    var nextDir = 0
-    //当前速度
-    var nowSpeed = 20
-    //当前长度
-    var nowLength = 100
-    var bodyLists = ArrayList<Point>()
 
 
     constructor(context: Context) : super(context) {
@@ -55,8 +61,10 @@ class EatView : View {
         (context as Activity).windowManager.defaultDisplay.getMetrics(dm)
         windowX = dm.widthPixels.toFloat()
         windowY = dm.heightPixels.toFloat()
+        mPointList= ArrayList()
         //起始位置在中心点
         position = Point((windowX / 2).toInt(), (windowY / 2).toInt())
+        mFoodPosition=Point()
         paint = Paint()
         paint.isAntiAlias = true
         paint.color = Color.BLACK
@@ -67,96 +75,115 @@ class EatView : View {
      * 开车
      */
     private fun didi() {
-        bodyLists.add(Point((position.x.toFloat() - eatWid / 2).toInt(), (position.y.toFloat() - nowLength).toInt()))
-        bodyLists.add(Point((position.x.toFloat() + eatWid / 2).toInt(), (position.y)))
+        mPointList.add(Point((position.x.toFloat()).toInt(), (position.y.toFloat() - eatWid).toInt()))
+        mPointList.add(Point((position.x.toFloat()).toInt(), (position.y)))
 
-//        val sec: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
-//        sec.scheduleAtFixedRate({
-//            post { invalidate() }
-//        }, 1000, 1000, TimeUnit.MILLISECONDS)
     }
 
     /**
-     * x法判断上下左右方向
+     * 判断方向的方法改为当前方向和触摸范围来判断
      */
-    fun xDir(x:Float,y:Float): Int {
-        if ((y / x) < (windowY / windowX)) {
-            //左上到右下线的右侧
-            if (((windowY - y) / x) <( windowY / windowX)) {
-                return 3
-            } else {
-                return 1
-            }
-
+    fun xDir(x: Float, y: Float) {
+        if (nowDir == UP || nowDir == DOWN) {
+            if (x <mPointList[0].x) nowDir = LEFT;
+            if (x > mPointList[0].x) nowDir = RIGHT;
         } else {
-            if (((windowY - y) / x )<( windowY / windowX)) {
-                return 0
-            } else {
-                return 2
-            }
+            if (y < mPointList[0].y) nowDir = UP;
+            if (y > mPointList[0].y) nowDir = DOWN;
         }
-
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                //检测到按下操作，确定当前方向，怎样判断呢，以X方式来判断
-                if (nowDir != xDir(event.x,event.y)) {
-                    //准备转弯
-                    nextDir = xDir(event.x,event.y)
-                    bodyLists.add(Point(bodyLists[bodyLists.size-1].x,bodyLists[bodyLists.size-1].y-20))
-                }
-                invalidate()
+                xDir(event.x, event.y)
             }
         }
         return super.onTouchEvent(event)
 
     }
 
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        if (bodyLists[1].x - bodyLists[0].x == 20) {
-            bodyLists[0].y += nowSpeed
+        mPointList.forEach { point: Point ->
+            val rect = Rect(point.x, point.y, point.x + eatWid, point.y + eatWid)
+            canvas.drawRect(rect, paint)
         }
-        if (nextDir != nowDir) {
-            when (nextDir) {
-                0 -> {
-                }
-                1 -> {
+        snakeMove(mPointList, nowDir)
+        //去掉最后一个
+        if (!eatFood) {
+            val foodrect = Rect(mFoodPosition.x, mFoodPosition.y, mFoodPosition.x + eatWid, mFoodPosition.y + eatWid)
+            val head = mPointList.get(0)
+            val headrect = Rect(head.x, head.y, head.x + eatWid, head.y + eatWid)
+            if (foodrect.intersect(headrect)) {
+                eatFood = true
+            } else {
+                mPointList.removeAt(mPointList.size - 1)
+            }
+        } else {
+            mPointList.removeAt(mPointList.size - 1)
+        }
+        drawFood(canvas,paint)
+    }
 
-                }
-                2 -> {
+    private fun drawFood(canvas: Canvas, paint: Paint) {
+        if (eatFood) {
+            mFoodPosition.x = mRandom.nextInt((windowX - 50 - eatWid).toInt())
+            mFoodPosition.y = mRandom.nextInt((windowY - 50 - eatWid).toInt())
+            eatFood = false
+        }
+        val food = Rect(mFoodPosition.x, mFoodPosition.y, mFoodPosition.x + eatWid, mFoodPosition.y + eatWid)
+        canvas.drawRect(food, paint)
 
-                }
-                3 -> {
-                    bodyLists[bodyLists.size - 1].x += nowSpeed
-                }
+    }
+
+    fun snakeMove(list: ArrayList<Point>, direction: Int) {
+        val orighead = list[0]
+        val newhead = Point()
+        when (direction) {
+            UP -> {
+                newhead.x = orighead.x
+                newhead.y = orighead.y - eatWid
+            }
+            DOWN -> {
+                newhead.x = orighead.x
+                newhead.y = orighead.y + eatWid
+            }
+            LEFT -> {
+                newhead.x = orighead.x - eatWid
+                newhead.y = orighead.y
+            }
+            RIGHT -> {
+                newhead.x = orighead.x + eatWid
+                newhead.y = orighead.y
+            }
+            else -> {
             }
         }
+        adjustHead(newhead)
+        list.add(0, newhead)
+    }
 
-        for (n in 0..bodyLists.size - 2) {
-            canvas.drawRect(bodyLists[n].x.toFloat(), bodyLists[n].y.toFloat(), bodyLists[n + 1].x.toFloat(), bodyLists[n + 1].y.toFloat(), paint)
+    private fun isOutBound(point: Point): Boolean {
+        if (point.x < 0 || point.x > windowX) return true
+        return if (point.y < 0 || point.y > windowY) true else false
+    }
+
+
+    private fun adjustHead(point: Point) {
+        if (isOutBound(point)) {
+            if (nowDir == UP) point.y = (windowY - eatWid).toInt()
+            if (nowDir == DOWN) point.y = (windowY / 2).toInt()
+            if (nowDir == LEFT) point.x = (windowX - eatWid).toInt()
+            if (nowDir == RIGHT) point.x = (windowX / 2).toInt()
         }
-
-//        when (nowDir) {
-//            0 -> {
-//
-//                position.y += nowSpeed
-//            }
-//            3 -> {
-//
-//            }
-//
-//        }
-
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        setMeasuredDimension(windowX.toInt(), windowY.toInt());
-
+        setMeasuredDimension(windowX.toInt(), windowY.toInt())
     }
-
 
 }
--- app/src/main/java/com/tao/habaoeat/MainActivity.kt
+++ app/src/main/java/com/tao/habaoeat/MainActivity.kt
@@ -2,11 +2,16 @@ package com.tao.habaoeat
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tsc = Executors.newSingleThreadScheduledExecutor()
+        tsc.scheduleAtFixedRate({ runOnUiThread { eatview.invalidate() } }, 200, 200, TimeUnit.MILLISECONDS)
     }
 }
